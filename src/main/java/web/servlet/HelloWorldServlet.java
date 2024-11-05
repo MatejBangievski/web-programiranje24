2b6f52ff6ab5228bcd2d754c602063b651954b41
MatejBangievski <matej.bangievski@gmail.com>
2024-11-05 11:09:42 +0100

    AUD 1

---
+++ src/main/java/web/servlet/HelloWorldServlet.java
@@ -0,0 +1,32 @@
+package web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        //Se zima od query stringot
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+
+        if (name == null || surname == null) {
+            name = "X";
+            surname = "Y";
+        }
+
+        writer.format("<html><head><body><h1>Hello %s %s!</h1></body></head></html>", name, surname);
+        writer.flush();
+
+    }
+}
---
+++ src/test/java/matejbangievski/webaud/WebAudApplicationTests.java
@@ -0,0 +1,13 @@
+package matejbangievski.webaud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
